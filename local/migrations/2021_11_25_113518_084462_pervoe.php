c1605cf77a877a91e5467b7730d8ff10e4fbdcb6
Siarhei <seregadixi@mail.ru>
2021-11-25 11:38:59 +0300

    z1

---
+++ local/migrations/2021_11_25_113518_084462_pervoe.php
@@ -0,0 +1,29 @@
+<?php
+
+use Arrilot\BitrixMigrations\BaseMigrations\BitrixMigration;
+use Arrilot\BitrixMigrations\Exceptions\MigrationException;
+
+class Pervoe20211125113518084462 extends BitrixMigration
+{
+    /**
+     * Run the migration.
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migration.
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function down()
+    {
+        //
+    }
+}
